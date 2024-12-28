0da9aaa5d0eaac95f5bbc3f43daa9960f3feda7b
LeeGodSRC <lee20040919@gmail.com>
2024-12-28 20:21:47 +0800

    chore: Bumped version to 0.7.11b1-SNAPSHOT chore: Bumped adventure version to 4.18.0

--- framework/platforms/mc-platform/build.gradle.kts
+++ framework/platforms/mc-platform/build.gradle.kts
@@ -7,13 +7,13 @@ dependencies {
     compileOnly("io.netty:netty-all:4.1.100.Final")
 
     // Adventure, for user-interface
-    api("net.kyori:adventure-api:4.17.0")
-    api("net.kyori:adventure-text-serializer-gson:4.17.0")
-    api("net.kyori:adventure-text-serializer-legacy:4.17.0")
-    api("net.kyori:adventure-nbt:4.17.0")
-    api("net.kyori:adventure-text-minimessage:4.17.0")
-    api("net.kyori:adventure-text-serializer-gson-legacy-impl:4.17.0")
-    api("net.kyori:adventure-text-serializer-plain:4.17.0")
+    api("net.kyori:adventure-api:4.18.0")
+    api("net.kyori:adventure-text-serializer-gson:4.18.0")
+    api("net.kyori:adventure-text-serializer-legacy:4.18.0")
+    api("net.kyori:adventure-nbt:4.18.0")
+    api("net.kyori:adventure-text-minimessage:4.18.0")
+    api("net.kyori:adventure-text-serializer-gson-legacy-impl:4.18.0")
+    api("net.kyori:adventure-text-serializer-plain:4.18.0")
 
     api("com.github.retrooper:packetevents-api:2.7.0") {
         exclude(group = "net.kyori")
